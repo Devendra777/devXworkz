5e209612161cbea8fc2aac170fc78276a318316e
Devendra777 <devendrasignh77@gmail.com>
2020-11-13 22:16:16 +0530

    prgone coming on the way

---
+++ ProgOne.java
@@ -0,0 +1,18 @@
+public class ProgOne
+{
+public static void main(String[] args)
+{
+int a[]={1,6,13};
+int b[]={5,9,3};
+for(int i=0;i<a.length;i++)
+{
+c[i]=a[i];
+}
+for(int j=0;j<b.length;j++)
+{
+c[i]=b[i];
+}
+System.out.println(c[i]);
+}
+}
+
