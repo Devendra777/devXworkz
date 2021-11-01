a68428821b3ae3ed2c1657ef88680953c7ca02dd
Devendra777 <devendrasignh77@gmail.com>
2021-11-01 14:32:57 +0530

    Committing code changes for crud with switch

--- Hotel.java
+++ Hotel.java
@@ -1,11 +1,11 @@
 class Hotel{
 
+  
 
  public FoodItemsDTO[] foodItems ;
 private int  index  ;
 
 public  Hotel(int size){
-
 foodItems = new   FoodItemsDTO[size];
 }
     
@@ -23,11 +23,82 @@ this.foodItems[index++]  =      foodItems;
 return isAdded ; 
 }
 
-public void getFoodItems(){
+public void getAllFoodItems(){
 for(int i= 0 ; i < foodItems.length ; i++){
+    if(foodItems[i] != null){
       System.out.println(foodItems[i].getFoodId() + "   "+  foodItems[i].getFoodName() + "   "+
                                              foodItems[i].getPrice() + "   "+   foodItems[i].getFoodType() );
 }
+
+}
+}
+
+
+
+    public  boolean updateFoodPriceByName(double price , String foodName ){
+    System.out.println("inside updateFoodPriceByName()");
+       boolean priceUpdated = false ;
+        if(price > 0.0 && foodName != null){
+              System.out.println("price is to be updated");
+              for(int i=0 ; i  < foodItems.length ; i++)
+               { 
+
+                                  
+                   if(foodItems[i].getFoodName().equals(foodName )){
+                                       
+                                  foodItems[i].setPrice(price);
+                                  priceUpdated = true;     
+                                                   
+                     }
+
+             }
+       }
+           return priceUpdated ; 
+
+   }
+
+
+
+     public boolean deleteFoodItemById(int id){
+       System.out.println("inside deleteFoodItemById()");
+        boolean isDeleted = false ; 
+     for(int i= 0 ; i  < foodItems.length ; i++)
+       {     
+            if(foodItems[i] != null){
+               
+          if(foodItems[i].getFoodId() == id){
+                            foodItems[i]= null;       
+                        isDeleted  = true;
+   
+         }
+}else{
+      System.out.println("Food Items  got Deleted ");
+        }
+
+
+      }
+           
+       return isDeleted ;
+     }
+
+
+
+
+  public  FoodItemsDTO   getFoodItemByFoodId(int id){
+   System.out.println("inside  getFoodItemByFoodId");
+      FoodItemsDTO dto = null;
+     for(int i = 0  ;  i < foodItems.length ; i++){
+      if(id  > 0 ){
+    
+          if( foodItems[i].getFoodId() == id)
+              dto        =      foodItems[i];
+    
+      }
+    else {
+System.out.println("id with " + id +  "   is not available");
+   }
+}
+  return dto ; 
 }
 
 }
--- HotelUtil.java
+++ HotelUtil.java
@@ -9,18 +9,64 @@ public static void main(String a[]){
    System.out.println("Enter the size");
        int size    =  sc.nextInt();
           Hotel hotel = new Hotel(size);
+  FoodItemsDTO dto1 ;
+      for(int i = 0 ; i < size ; i++){
+     dto1 = new FoodItemsDTO();
+  System.out.println("Enter the id");
+          dto1.setFoodId(sc.nextInt());
+ System.out.println("Enter the  Food Name");
+          dto1.setFoodName(sc.next());
+ System.out.println("Enter the  Food Price");
+          dto1.setPrice(sc.nextDouble());
+ System.out.println("Enter the  Food type");
+          dto1.setFoodType(sc.next());
+
+         hotel.addFoodItems(dto1);
+     }
+
+
+ String text = null ;
+   do{
+     System.out.println("Press 1 to get all Food Items");
+   System.out.println(" Press 2   to Update Food Price By Name");
+    System.out.println("Press 3   to Delete Food Item By id");
+   System.out.println("Press 4 to fetch Food Item by  id") ;
+
+       int choice =   sc.nextInt();
+    
+switch(choice){
+      case 1  :      hotel.getAllFoodItems();
+      break;
+      case 2  :   System.out.println("Enter the food Name to update Price");
+                        String foodName = sc.next();
+                        System.out.println("Enter the Updated Price ");
+                       double price = sc.nextDouble();
+                       boolean isUpdated =  hotel. updateFoodPriceByName(price , foodName) ; 
+                       System.out.println("Price is Updated "+isUpdated);
+      break ; 
+
+   case 3  :    System.out.println("Enter the  Id to delete Food Item");
+                     hotel.deleteFoodItemById(sc.nextInt());
+     break ; 
+   case 4  :   System.out.println("Enter the  Id to get Food Item");
+    FoodItemsDTO   dto  =    hotel. getFoodItemByFoodId(sc.nextInt()) ; 
+      if(dto != null){
+              System.out.println(dto.getFoodId() + "   "+ dto.getFoodName()  + "  "+  dto.getPrice() + "  "+ dto .getFoodType());
+       }
+else{
+         System.out.println("No Food Found");
+    }
+   break ; 
+    default :   System.out.println("Wrong Choice ...... ");
+ break  ; 
+}
+   
+  System.out.println(" Do u want to continue ?  Y/N");
+   text =  sc.next();
+ }while(text.equals("Y")) ;
   
-     FoodItemsDTO dto  = new FoodItemsDTO();
-          dto.setFoodId(1);
-          dto.setFoodName("Pasta");
-          dto.setPrice(106.00);
-          dto.setFoodType("Chinese");
+  System.out.println("Thank you for Using the Food Items Application");
 
-     
-hotel.addFoodItems(dto);
-
-   hotel.getFoodItems();
-       
 
 }
 
